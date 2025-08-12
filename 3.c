cc5162d7b51c6bc320b46f8f041ff65e1a967c69
Guilherme Anzollin <guilherme.anzollin@codelabore.com>
2025-08-11 21:32:58 -0300

    Atividade 3 parcial

---
+++ 3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+typedef struct {
+    float altura;
+    float largura;
+    float profundidade;
+} REGISTRO;
+
+void inserir(REGISTRO *p) {
+    float altura, largura, profundidade;
+    puts("informe a altura");
+    scanf("%f", &altura);
+    puts("informe a largura");
+    scanf("%f", &largura);
+    puts("informe a profundidade");
+    scanf("%f", &profundidade);
+    // salvar o valor das variáveis em registros
+}
+
+void main () {
+    REGISTRO registros[10];
+    int opcao;
+
+    while(opcao != 5) {
+        puts("Escolha uma opcao a seguir:");
+        puts("1. Inserir registro");
+        puts("2. Alterar registro");
+        puts("3. Excluir registro");
+        puts("4. Mostrar registro");
+        puts("5. Sair");
+        scanf("%i", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            inserir(registros);
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        }
+    }
+}
